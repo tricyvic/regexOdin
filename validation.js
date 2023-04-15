c9ddcf99702f6384b28908868ff743f617732cbd
Victor Muhita <muhitavictor69@gmail.com>
2023-04-15 23:28:06 +0300

    testing regex

---
+++ validation.js
@@ -0,0 +1,20 @@
+const inputs =document.querySelectorAll("input")
+
+const patterns = {
+    telephone: /^\d{10}$/
+}
+function validate(field,regex){
+   if(regex.test(field.value)){
+    field.className = 'valid';
+   }else{
+    field.className = 'invalid';
+   }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener("keyup",(e)=>{
+        // console.log(e.target.attributes.name.value)
+        validate(e.target,patterns[e.target.attributes.name.value])
+    })
+})
+
